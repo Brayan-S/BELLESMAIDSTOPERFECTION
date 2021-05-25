dc444ca0310a34c8d974ab0443085f8ec4b60d83
Brayan Solis <brayan.develope777@gmail.com>
2021-05-25 16:14:20 -0500

    bootstrap

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c86898623a4ea305ae2dcd57f6777277bc3b75c2',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c86898623a4ea305ae2dcd57f6777277bc3b75c2',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.3.1',
+      'version' => '4.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8fa0d3010112dca5dd6dd501173415856001ba8b',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.3.1',
+      ),
+    ),
+  ),
+);
